94b4116ca2c622f633d4e23abc90bf92130b4973
harsh <harshprakash903@gmail.com>
2025-09-07 21:36:00 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Hero from "./components/hero";
+import Features from "./components/features";
+import Product from "./components/product";
+import './App.css';
+import jam3 from "./assets/jam3.png";
+import Challenges from "./components/challenges";
+import Footer from "./components/footer";
+import Technology from "./components/technology.jsx";
+
+
+function App() {
+  return (
+    <div className="background-image">
+      <img src={jam3} alt="Traffic Background" className="bg-image" />
+      
+
+      <div className="content-overlay">
+        <Navbar />
+        <Hero />
+        <Product />
+        <Features />
+        <Challenges/>
+        <Technology />
+        <Footer />
+      </div>
+      
+    </div>
+  )
+}
+
+export default App;
